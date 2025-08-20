015688fa7ebc4f19571ae0280d2270d5d5dfc342
MrSansource <baptistesalzer@orange.fr>
2025-08-20 14:43:05 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import {
   DndContext,
   DragOverlay,
